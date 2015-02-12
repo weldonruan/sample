0664da5eb50d74c19873543a04c6fb36a6ad49e8
阮炜 <ruanwei@ruanweideiMac.local>
2015-02-12 11:29:12 +0800

    urlview OK

--- url/ViewController.swift
+++ url/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var wv:UIWebView?;
+    @IBOutlet var vi:UIView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 测试
+        //println( vi
+        // 修改UIWebView的属性
+        var uu  = NSURL(string: "http://map.baidu.com")
+        var req = NSURLRequest(URL:uu!)
+        wv!.sizeToFit()
+        wv!.loadRequest(req)
     }
 
     override func didReceiveMemoryWarning() {
