426d9d9d4e303049931cdef3349bbd0f86bad7fb
阮炜 <ruanwei@ruanweideiMac.local>
2015-02-12 17:22:43 +0800

    2nd commit

---
+++ readme.txt
@@ -0,0 +1,3 @@
+第二个可执行的ios程序
+
+程序中创建UIWebView空间，定义控件尺寸并显示网址：map.baidu.com
--- url/ViewController.swift
+++ url/ViewController.swift
@@ -10,17 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var wv:UIWebView?;
     @IBOutlet var vi:UIView?
+    var wv : UIWebView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // 测试
-        //println( vi
-        // 修改UIWebView的属性
+        
+        // 获取内容区长宽
+        var sc_width = vi!.frame.width
+        var sc_height = vi!.frame.height
+        println( "UIView width=\(sc_width) , height=\(sc_height)" )
+        
+        // 创建UIWebView
+        wv = UIWebView(frame: CGRect(x:0,y:0,width:sc_width,height:sc_height))
+        self.view.addSubview(wv!)
+        println( "UIWebView width=\(wv!.frame.width) , height=\(wv!.frame.height)" )
+        //打开网页
         var uu  = NSURL(string: "http://map.baidu.com")
         var req = NSURLRequest(URL:uu!)
-        wv!.sizeToFit()
         wv!.loadRequest(req)
     }
 
